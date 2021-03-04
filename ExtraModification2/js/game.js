0e7f8c4e83b9afb8ae5c674a73a06c31239be3f5
ArshiaSaleem98 <arshiasaleem98@gmail.com>
2021-03-04 19:52:12 +0100

    Level2 Done

--- ExtraModification2/js/game.js
+++ ExtraModification2/js/game.js
@@ -358,21 +358,32 @@ var levels = {
             entities: [
                 { type: "ground", name: "dirt", x: 500, y: 440, width: 1000, height: 20, isStatic: true },
                 { type: "ground", name: "wood", x: 185, y: 390, width: 30, height: 80, isStatic: true },
-
-                { type: "block", name: "wood", x: 820, y: 380, angle: 90, width: 100, height: 25 },
-                { type: "block", name: "wood", x: 720, y: 380, angle: 90, width: 100, height: 25 },
+                  
+                { type: "block", name: "wood", x: 520, y: 380, angle: 90, width: 100, height: 25 },
+                { type: "block", name: "glass", x: 570, y: 317.5, width: 124, height: 25 },
                 { type: "block", name: "wood", x: 620, y: 380, angle: 90, width: 100, height: 25 },
-                { type: "block", name: "glass", x: 670, y: 317.5, width: 100, height: 25 },
-                { type: "block", name: "glass", x: 770, y: 317.5, width: 100, height: 25 },
-                { type: "block", name: "glass", x: 670, y: 255, angle: 90, width: 100, height: 25 },
-                { type: "block", name: "glass", x: 770, y: 255, angle: 90, width: 100, height: 25 },
-                { type: "block", name: "wood", x: 720, y: 192.5, width: 100, height: 25 },
-                { type: "villain", name: "burger", x: 715, y: 155, calories: 590 },
-                { type: "villain", name: "fries", x: 670, y: 405, calories: 420 },
-                { type: "villain", name: "sodacan", x: 765, y: 400, calories: 150 },
-                { type: "hero", name: "strawberry", x: 30, y: 415 },
-                { type: "hero", name: "orange", x: 80, y: 405 },
-                { type: "hero", name: "apple", x: 140, y: 405 },
+                { type: "block", name: "wood", x: 520, y: 250, angle: 90, width: 100, height: 25 },
+                { type: "block", name: "glass", x: 570, y:200, width: 124, height: 25 },
+                { type: "block", name: "wood", x: 620, y: 250, angle: 90, width: 100, height: 25 },
+
+                { type: "block", name: "wood", x: 680, y: 380, angle: 90, width: 100, height: 25 },
+                { type: "block", name: "glass", x: 730, y: 317.5, width: 124, height: 25 },
+                { type: "block", name: "wood", x: 780, y: 380, angle: 90, width: 100, height: 25 },
+                { type: "block", name: "wood", x: 680, y: 250, angle: 90, width: 100, height: 25 },
+                { type: "block", name: "glass", x: 730, y:200, width: 124, height: 25 },
+                { type: "block", name: "wood", x: 780, y: 250, angle: 90, width: 100, height: 25 },
+
+                { type: "villain", name: "villain1", x: 570, y: 405, calories: 420 },
+                { type: "villain", name: "villain2", x: 570, y: 280, calories: 420 },
+                { type: "villain", name: "villain1", x: 730, y: 280, calories: 420 },
+                { type: "villain", name: "villain2", x: 730, y: 405, calories: 420 },
+                { type: "villain", name: "villain1", x: 570, y: 155, calories: 420 },
+                { type: "villain", name: "villain2", x: 730, y: 155, calories: 420 },
+
+            
+                { type: "hero", name: "hero1", x: 30, y: 415 },
+                { type: "hero", name: "hero2", x: 80, y: 405 },
+                { type: "hero", name: "hero1", x: 140, y: 405 },
             ]
         },
         {//third level
